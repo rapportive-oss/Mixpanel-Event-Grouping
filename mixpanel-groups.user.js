11f66650e64835f935fc7ba5286fdb3491222f32
Andrew Brown <andrew@rapportive.com>
2011-06-23 20:50:23 -0500

    actually, do a fuzzy search on the event name rather than a strict format

--- mixpanel-groups.user.js
+++ mixpanel-groups.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name          Mixpanel Event Grouping
 // @namespace     http://mixpanel.com
-// @description	  Group Mixpanel events by topic in <Topic>: <Description>
+// @description	  Group Mixpanel events by topic
 // @include       http://mixpanel.com/report/*/events*
 // @include       http://www.mixpanel.com/report/*/events*
 // ==/UserScript==
@@ -13,7 +13,7 @@ document.onkeypress = function (key) {
     }
 
     // Ask the user what group of events they'd like to view
-    var group = prompt('What group of events would you like to view?', '');
+    var group = prompt('Display events with the following phrase:', '').toLowerCase();
 
     // Do nothing if the prompt was cancelled
     if (group === null) {
@@ -22,16 +22,16 @@ document.onkeypress = function (key) {
 
     // Iterate through all the event rows on the page and hide the ones that aren't
     // part of the currently-viewed group
-    events = document.getElementsByClassName('event_row');
+    var events = document.getElementsByClassName('event_row');
     for (var i = 0; i < events.length; i++) {
-        event_name = events[i].getElementsByTagName('td')[1].innerText.split(':')[0];
+        var event_name = events[i].getElementsByTagName('td')[1].innerText.toLowerCase();
         
         // Show a row if it's topic matches the group we want to show. Otherwise hide it.
-        if (group === event_name || group === '') {
+        if (event_name.indexOf(group) > -1 || group === '') {
             events[i].style.display = 'table-row';
         } else {
             events[i].style.display = 'none';
         }
     }
 
-}
+};
